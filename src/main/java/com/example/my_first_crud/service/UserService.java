4efcab4afc04e4b7f21c122e6cc98c1e136cf6b6
GnezdilovaVO <valyapomortseva@gmail.com>
2024-04-03 20:54:32 +0700

    Homework_2

---
+++ src/main/java/com/example/my_first_crud/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.my_first_crud.service;
+
+import com.example.my_first_crud.model.User;
+import com.example.my_first_crud.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+}
